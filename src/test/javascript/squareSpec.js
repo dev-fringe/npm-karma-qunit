4a1d410b5b76a2219e6df1330e96e23a60fc1e71
LEE HAK DO <dev.fringe@gmail.com>
2016-06-13 18:00:58 +0900

    Update squareSpec.js
    change

--- src/test/javascript/squareSpec.js
+++ src/test/javascript/squareSpec.js
@@ -2,12 +2,8 @@ fixture.setBase('src/main/java/webapp');
 QUnit.module('DOM manipulation', {
     beforeEach: function() {
         fixture.load('index.html');
-         $('#jspFlightNumber').val('KE603');
-         $('#jspSchDepD').val('2016-02-10');
-
     }
 });
 QUnit.test("hello test", function( assert ) {
-    fn_getPassengerList()
     assert.ok( square(3) == "9", "Passed!" );
 });
